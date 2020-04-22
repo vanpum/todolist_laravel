921c1f7989d08d5df965a56ff73d49ab3730fbeb
Shin Min Seo <vanpumshs@naver.com>
2020-04-22 21:34:19 +0900

    데이터베이스 저장 해 보기

--- app/Http/Controllers/TodolistController.php
+++ app/Http/Controllers/TodolistController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TodolistController extends Controller
 {
@@ -12,11 +14,20 @@ class TodolistController extends Controller
         return "리스트 컨트롤러";
     }
 
-    public function store()
+    public function store(Request $request)
     {
-        return "글저장 컨트롤러";
+        $value_array = $request->toArray();
+        DB::insert('insert into todolists (title, contents, writer, status, progress, created_at) values (?, ?, ?, ?, ?, ?)', 
+        [$value_array['title'],
+        $value_array['contents'],
+        $value_array['writer'],
+        $value_array['status'],
+        $value_array['progress'],
+        $value_array['created_at']]);
+        
+        return ['result'=>true];
     }
-    
+
     public function remove()
     {
         return "글 삭제 컨트롤러";
